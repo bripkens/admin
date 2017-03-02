d150acfc3550f09130630a101c12facb02feb669
Ben Ripkens <bripkens@gmail.com>
2017-03-02 21:41:31 +0100

    Support auto format

--- test/config/validator.test.js
+++ test/config/validator.test.js
@@ -1,7 +1,7 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 const Ajv = require('ajv');
 
-const {validate} = require('../../lib/config/validator');
+const { validate } = require('../../lib/config/validator');
 const schema = require('../../lib/config/schema.json');
 
 describe('config/validator', () => {
@@ -16,20 +16,20 @@ describe('config/validator', () => {
       }
     });
     expect(result.valid).to.equal(true);
-    expect(result.message).to.equal('')
+    expect(result.message).to.equal('');
   });
 
   describe('missing properties', () => {
     it('must fail when required property admin is not defined', () => {
       const result = validate(schema, {});
       expect(result.valid).to.equal(false);
-      expect(result.message).to.contain('- should have required property \'admin\' at path .')
+      expect(result.message).to.contain("- should have required property 'admin' at path .");
     });
 
     it('must fail when required property admin.port is not defined', () => {
-      const result = validate(schema, {admin: {}});
+      const result = validate(schema, { admin: {} });
       expect(result.valid).to.equal(false);
-      expect(result.message).to.contain('- should have required property \'port\' at path .admin')
+      expect(result.message).to.contain("- should have required property 'port' at path .admin");
     });
   });
 });
