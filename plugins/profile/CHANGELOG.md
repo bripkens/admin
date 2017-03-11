2d29276b6569a72fadd6ec326f8017ebe18d50a8
Ben Ripkens <bripkens@gmail.com>
2017-03-11 18:45:37 +0100

    Use `@risingstack/v8-profiler` for Node.js v7 support

--- plugins/profile/CHANGELOG.md
+++ plugins/profile/CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## Unreleased
+ - Use `@risingstack/v8-profiler` for Node.js v7 support.
+
 ## 1.0.0
  - Initial release
--- plugins/profile/index.js
+++ plugins/profile/index.js
@@ -1,62 +1,69 @@
-// Ensure that users of admin-plugin-profile don't get more than they bargained for.
-// Specifically, we don't want heapdump to register its signal handlers.
-if (process.env.NODE_HEAPDUMP_OPTIONS == null) {
-  process.env.NODE_HEAPDUMP_OPTIONS = 'nosignal';
-}
-const heapdump = require('heapdump');
-
 const router = require('express').Router();
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
-let logger;
+module.exports = () => api => {
+  const logger = api.getLogger('admin-plugin-profile');
 
-router.get('/', (req, res) => {
-  res.render(path.join(__dirname, 'index.html'));
-});
+  let profiler;
+  try {
+    profiler = require('@risingstack/v8-profiler');
+  } catch (e) {
+    logger.error('v8-profiler could not be loaded. Will not expose profiling endpoints', e);
+    return {
+      name: 'profile',
+      mountPath: '/profile',
+      navigationItems: [],
+      router
+    };
+  }
 
-router.get('/heapdump', (req, res) => {
-  const filename = `${Date.now()}.heapsnapshot`;
-  const targetPath = path.join(os.tmpdir(), filename);
-  logger.info('Heap dump initiated via admin endpoint. Collecting heap dump to %s', targetPath);
+  router.get('/', (req, res) => {
+    res.render(path.join(__dirname, 'index.html'));
+  });
 
-  heapdump.writeSnapshot(targetPath, err => {
-    if (err) {
-      logger.warn('Failed to write heap dump to disk', err);
-      res.status(500).send(`Failed to write heap snapshot to ${targetPath}: ${err}`);
-      attemptHeapdumpCleanup(targetPath);
-      return;
-    }
+  router.get('/heapdump', (req, res) => {
+    const filename = `${Date.now()}.heapsnapshot`;
+    const targetPath = path.join(os.tmpdir(), filename);
+    logger.info('Heap dump initiated via admin endpoint. Collecting heap dump to %s', targetPath);
 
-    logger.info('Heap dump written to disk, forwarding to user');
-    res
-      .set('Content-Disposition', `attachment; filename="${filename}"`)
-      .set('Content-Type', 'text/plain')
-      .sendFile(targetPath, err => {
-        if (err) {
-          logger.warn('Failed to send heap dump to user', err);
-        }
+    const snapshot = profiler.takeSnapshot();
+    snapshot.export()
+      .pipe(fs.createWriteStream(targetPath))
+      .on('error', err => {
+        logger.warn('Failed to write heap dump to disk', err);
+        res.status(500).send(`Failed to write heap snapshot to ${targetPath}: ${err}`);
         attemptHeapdumpCleanup(targetPath);
+        snapshot.delete();
+      })
+      .on('finish', () => {
+        logger.info('Heap dump written to disk, forwarding to user');
+        res
+          .set('Content-Disposition', `attachment; filename="${filename}"`)
+          .set('Content-Type', 'text/plain')
+          .sendFile(targetPath, err => {
+            if (err) {
+              logger.warn('Failed to send heap dump to user', err);
+            }
+            attemptHeapdumpCleanup(targetPath);
+          });
+        snapshot.delete();
       });
   });
-});
 
-function attemptHeapdumpCleanup (path) {
-  try {
-    fs.unlinkSync(path);
-  } catch (e) {
-    if (e.code !== 'ENOENT') {
-      logger.warn('Failed to clean up heap dump at path %s', path, e);
+  function attemptHeapdumpCleanup (path) {
+    try {
+      fs.unlinkSync(path);
+    } catch (e) {
+      if (e.code !== 'ENOENT') {
+        logger.warn('Failed to clean up heap dump at path %s', path, e);
+      }
     }
   }
-}
-
-module.exports = () => api => {
-  logger = api.getLogger('admin-plugin-profile');
 
   return {
-    name: 'index',
+    name: 'profile',
     mountPath: '/profile',
     navigationItems: [
       {
