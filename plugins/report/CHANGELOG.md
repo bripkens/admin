e97d7e0da477e560106599804e292d9679926be0
Ben Ripkens <bripkens@gmail.com>
2017-06-08 07:30:44 -0700

    report: Do not abort application startup when `node-report` cannot be loaded.

--- plugins/report/CHANGELOG.md
+++ plugins/report/CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 1.0.1
+ - Do not abort application startup when `node-report` cannot be loaded.
+
 ## 1.0.0
  - Initial release
--- plugins/report/index.js
+++ plugins/report/index.js
@@ -1,18 +1,32 @@
-const nodeReport = require('node-report/api');
 const router = require('express').Router();
 const path = require('path');
 
-router.get('/', (req, res) => {
-  if (req.accepts('html')) {
-    res.render(path.join(__dirname, 'index.html'), {
-      report: nodeReport.getReport()
-    });
-  } else {
-    res.send(nodeReport.getReport());
-  }
-});
+module.exports = () => api => {
+  const logger = api.getLogger('admin-plugin-report');
 
-module.exports = () => () => {
+  let nodeReport;
+  try {
+    nodeReport = require('node-report/api');
+  } catch (e) {
+    logger.error('node-report could not be loaded. Will not expose reporting endpoint. Error: %s', e.message);
+    return {
+      name: 'report',
+      mountPath: '/report',
+      navigationItems: [],
+      router
+    };
+  }
+
+  router.get('/', (req, res) => {
+    if (req.accepts('html')) {
+      res.render(path.join(__dirname, 'index.html'), {
+        report: nodeReport.getReport()
+      });
+    } else {
+      res.send(nodeReport.getReport());
+    }
+  });
+
   return {
     name: 'report',
     mountPath: '/report',
